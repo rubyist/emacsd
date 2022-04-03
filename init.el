1e146d579c4764b2777b388313bf2eaf09c35bee
Scott Barron <scott@barron.io>
2022-04-02 20:43:23 -0400

    smarter ctrl a

--- init.el
+++ init.el
@@ -313,6 +313,29 @@
 (setq elfeed-feeds
       '("https://planet.emacslife.com/atom.xml"))
 
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first. If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+(global-set-key [remap move-beginning-of-line]
+		'smarter-move-beginning-of-line)
+
 (use-package nano-theme)
 (nano-light)
 (use-package nano-modeline)
